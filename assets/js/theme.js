c3550e36f9e517be487a1a6a50fdd4ebe795053e
Andre Lanrezac <lanrezac@ibpc.fr>
2024-11-08 15:27:00 +0100

    Remove system time option for dark/light toggle

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -3,12 +3,19 @@
 // Toggle through light, dark, and system theme settings.
 let toggleThemeSetting = () => {
   let themeSetting = determineThemeSetting();
-  if (themeSetting == "system") {
+  // if (themeSetting == "system") {
+  //   setThemeSetting("light");
+  // } else if (themeSetting == "light") {
+  //   setThemeSetting("dark");
+  // } else {
+  //   setThemeSetting("system");
+  // }
+  if (themeSetting == "dark") {
     setThemeSetting("light");
   } else if (themeSetting == "light") {
     setThemeSetting("dark");
   } else {
-    setThemeSetting("system");
+    setThemeSetting("light");
   }
 };
 
